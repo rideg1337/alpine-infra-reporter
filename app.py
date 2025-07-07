a7e67caf2ad13ad3c7ed87ed567cac730b68d435
Zsolt Rideg <r1deg@protonmail.com>
2025-07-07 13:06:38 +0200

    updated python script

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def get_system_info():
         "cpu_percent": psutil.cpu_percent(interval=1),
         "memory_percent": psutil.virtual_memory().percent,
         "disk_percent": psutil.disk_usage('/').percent,
-        "ip_adress" : socket.gethostbyname()
     }
 
 @app.route('/status')
