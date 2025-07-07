f2c9e34b4ae2745ac255f770f859b09d93842565
Zsolt Rideg <r1deg@protonmail.com>
2025-07-07 12:42:18 +0200

    Added .sh install script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+apk update
+apk add --no-cache git docker docker-cli containerd
+
+service docker start
+
+rc-update add docker boot
+
+mkdir -p /root/home/reporter
+cd /root/home/reporter
+
+if [ ! -d "alpine-infra-reporter" ]; then
+  git clone https://github.com/rideg1337/alpine-infra-reporter.git
+fi
+
+cd alpine-infra-reporter
+
+DOCKER_BUILDKIT=0 docker build -t reporter .
+
+docker run -p 5055:5055 reporter
+
+
